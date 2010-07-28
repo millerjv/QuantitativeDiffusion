6764adaf1cc5622d982a1b8da39ab305adaab997
Mahnaz Maddah <mmaddah@alum.mit.edu>
2010-07-28 23:55:24 +0000

    in progress

--- EMClustering/EMCMedialSheet.cxx
+++ EMClustering/EMCMedialSheet.cxx
@@ -111,11 +111,38 @@ bool  hasNoTouchingFace(std::vector <std::vector<int> > linkLists, vtkIdList* pt
 {
 	bool hasNoNei;
 	std::vector<int> l1,l2,l3,l0;
-	l0= linkLists.at(ptsIds->GetId(facePIds[0]));
-	l1= linkLists.at(ptsIds->GetId(facePIds[1]));
-	l2= linkLists.at(ptsIds->GetId(facePIds[2]));
-	l3= linkLists.at(ptsIds->GetId(facePIds[3]));
+	int id[4];
+	id[0] = ptsIds->GetId(facePIds[0]);
+	id[1] = ptsIds->GetId(facePIds[1]);
+	id[2] = ptsIds->GetId(facePIds[2]);
+	id[3] = ptsIds->GetId(facePIds[3]);
+	// if the vertices are not unique, repeated link list should be avoided
 
+	l0= linkLists.at(id[0]);
+	if (id[1]!=id[0])
+	{
+		l1= linkLists.at(id[1]);
+	}
+	else
+	{
+		l1.push_back(-1);
+	}
+	if (id[2]!=id[0] && id[2]!=id[1])
+	{
+	    l2= linkLists.at(id[2]);
+	}
+	else
+	{
+		l2.push_back(-1);
+	}
+	if (id[3]!=id[0] && id[3]!=id[1] && id[3]!=id[2])
+	{
+		l3= linkLists.at(id[3]);
+	}
+	else
+	{
+		l3.push_back(-1);
+	}
 	if (isEqual(l0,l1) || isEqual(l0,l2) || isEqual(l0,l3) || isEqual(l1,l2) || isEqual(l1,l3) || isEqual(l2,l3))
 	    hasNoNei = 0;
 	else //unique
@@ -144,7 +171,7 @@ int main(int argc, char* argv[])
 	MeshSourceType::Pointer midSurface = MeshSourceType::New();
 
 	//std::string filename ="/fs/corpus1/mahnaz/vtkMRMLFiniteElementMeshNode1.vtk";
-	std::string filename ="/fs/corpus1/mahnaz/cc.vtk";
+	std::string filename ="/fs/corpus1/mahnaz/dtiCode/MORIAtlas_ROIs/IA-FEMesh-Autosave-20100728-1114/Model_4_4_VMesh-5.vtk";
 
 
     vtkUnstructuredGridReader *reader = vtkUnstructuredGridReader::New();
@@ -196,29 +223,30 @@ int main(int argc, char* argv[])
 		int face6PIds[4] = {1,2,6,5};
 		bool face6 =  hasNoTouchingFace(linkLists, ptsIds, face6PIds);
 		bool validMesh=1;
-
+		std::cout << face1 << " " << face2 << " " << face3 << " " << face4 << " " <<face5 << " " << face6 <<std::endl;
 		MeshType::PointType vp0, vp1, vp2, vp3;
 		MeshType::PointType p0, p1, p2, p3;
-		if (face1 && face2)  //{{0,1,2,3},{4,5,6,7}}
-		{
-			vp0 = ave(points->GetPoint(0), points->GetPoint(4));
-			vp1 = ave(points->GetPoint(1), points->GetPoint(5));
-			vp2 = ave(points->GetPoint(2), points->GetPoint(6));
-			vp3 = ave(points->GetPoint(3), points->GetPoint(7));
-		}
-		else if (face3 && face4)  //{{0,1,5,4},{3,2,6,7}}
+		if (face3 && face4)  //{{0,1,5,4},{3,2,6,7}}
 		{
 			vp0 = ave(points->GetPoint(0), points->GetPoint(3));
 			vp1 = ave(points->GetPoint(1), points->GetPoint(2));
 			vp2 = ave(points->GetPoint(5), points->GetPoint(6));
 			vp3 = ave(points->GetPoint(4), points->GetPoint(7));
 		}
+		else if (face1 && face2)  //{{0,1,2,3},{4,5,6,7}}
+		{
+			vp0 = ave(points->GetPoint(0), points->GetPoint(4));
+			vp1 = ave(points->GetPoint(1), points->GetPoint(5));
+			vp2 = ave(points->GetPoint(2), points->GetPoint(6));
+			vp3 = ave(points->GetPoint(3), points->GetPoint(7));
+		}
+
 		else if (face5 && face6)  //{{0,3,7,4},{1,5,6,2}}
 		{
 			vp0 = ave(points->GetPoint(0), points->GetPoint(1));
 			vp1 = ave(points->GetPoint(3), points->GetPoint(2));
-			vp2 = ave(points->GetPoint(5), points->GetPoint(4));
-			vp3 = ave(points->GetPoint(6), points->GetPoint(7));
+			vp2 = ave(points->GetPoint(6), points->GetPoint(7));
+			vp3 = ave(points->GetPoint(5), points->GetPoint(4));
 		}
 		else
 		{
@@ -246,15 +274,29 @@ int main(int argc, char* argv[])
 		//std::cout << p2[0] << " " << p2[1] << " "<< p2[2] << std::endl;
 		//std::cout << p3[0] << " " << p3[1] << " "<< p3[2] << std::endl;
 		midSurface->AddQuadrilateral (midSurface->AddPoint(p1) , midSurface->AddPoint(p0),midSurface->AddPoint(p2), midSurface->AddPoint(p3));
-		//std::cout << midSurface->GetOutput()->GetNumberOfCells() << std::endl;
+		//int currentPoint =  midSurface->GetOutput()->GetNumberOfPoints()-1;
+		//if (p0[2] >= 14.0212 || p1[2] >= 14.0212 || p2[2] >= 14.0212 || p3[2] >= 14.0212)
+		if (c==275)
+		{
+			std::cout << c << std::endl;
+			std::cout << ptsIds->GetId(0) << " " << ptsIds->GetId(1) << std::endl;
+			std::cout << ptsIds->GetId(2) << " " << ptsIds->GetId(3) << std::endl;
+			std::cout << ptsIds->GetId(4) << " " << ptsIds->GetId(5) << std::endl;
+			std::cout << ptsIds->GetId(6) << " " << ptsIds->GetId(7) << std::endl;
+			std::cout << face1 << " " << face2 << " " << face3 << " " << face4 << " " <<face5 << " " << face6 <<std::endl;
+			std::cout << p0[0] << " " << p0[1] << " "<< p0[2] << std::endl;
+			std::cout << p1[0] << " " << p1[1] << " "<< p1[2] << std::endl;
+			std::cout << p2[0] << " " << p2[1] << " "<< p2[2] << std::endl;
+			std::cout << p3[0] << " " << p3[1] << " "<< p3[2] << std::endl;
+		}
 		}
 	}
 
 	CopyFieldType copyfield ={0,0,0,0,0,0};
-	WriteVTKfile(midSurface->GetOutput(), "/fs/corpus1/mahnaz/surface.vtk", copyfield);
+	WriteVTKfile(midSurface->GetOutput(), "/fs/corpus1/mahnaz/surface.vtp", copyfield);
 
     //midSurface->Print(std::cout);
-    midSurface->GetOutput()->Print(std::cout);
+    //midSurface->GetOutput()->Print(std::cout);
 	reader->Delete();
 
 
